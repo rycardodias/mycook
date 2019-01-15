a385a2fbfd3148d1d0653f096d4bf57072f6daa7
rycardodias <42947771+rycardodias@users.noreply.github.com>
2019-01-15 20:09:04 +0000

    .

--- resources/views/posts/ingredientes/index.blade.php
+++ resources/views/posts/ingredientes/index.blade.php
@@ -3,44 +3,44 @@
 <div class="nav">
     @include('includes.navbar')
 </div>
-
-@foreach($lista as $lista)
-<p><a href="/origem/{{$lista->id}}">{{$lista->origemAlimento}}</a>
-    @endforeach
 <section class="corpo" id="ingredientesIndex">
-    <div class="direita">
-
-                <a href="/ingredientes/create">Criar Receita</a>
-<h1>Lista de Ingredientes</h1>
-@if(count($ingredientes)>0)
-    <table>
-        <tr>
-            <th>Ingrediente</th>
-            <th>Descrição</th>
-            <th>Origem</th>
-            <th>Ver mais</th>
-        </tr>
-    @foreach($ingredientes as $ingrediente)
-            <tr>
-                <td>{{$ingrediente->nome}}</td>
-                <td>{{$ingrediente->descricao}}</td>
-                <td>{{$ingrediente->origemAlimento}}</td>
-                <td><a href="/ingredientes/{{$ingrediente->id}}">Ver mais</a></td>
-
-            </tr>
+    <div class="esquerda">
+        <div class="filtroORigemAlimento">
+                <h3>Origem Alimentos</h3>
+                <div class="opcao">
+            @foreach($lista as $lista)
+                <p><a href="/origem/{{$lista->id}}">{{$lista->origemAlimento}}</a>
             @endforeach
-        </table>
-
-@else
-    <p>Sem Ingredientes encontrados</p>
-@endif
+                    <p><a href="/ingredientes/create">Criar Receita</a>
+        </div>
+    </div>
+    </div>
+    <div class="direita">
+        <h1>Lista de Ingredientes</h1>
+        @if(count($ingredientes)>0)
+            <table>
+                <tr>
+                    <th>Ingrediente</th>
+                    <th>Descrição</th>
+                    <th>Origem</th>
+                    <th>Ver mais</th>
+                </tr>
+            @foreach($ingredientes as $ingrediente)
+                <tr id="lista">
+                    <td>{{$ingrediente->nome}}</td>
+                    <td>{{$ingrediente->descricao}}</td>
+                    <td>{{$ingrediente->origemAlimento}}</td>
+                    <td><a href="/ingredientes/{{$ingrediente->id}}">Ver mais</a></td>
+                </tr>
+            @endforeach
+            </table>
+        @else
+            <p>Sem Ingredientes encontrados</p>
+        @endif
     </div>
 </section>
-
-
 </div>
-<div class="esquerda">
-</div>
+
 
 <footer id="rodape">
     @include('includes.footer')
